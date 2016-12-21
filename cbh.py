4bedff26245e4802bbad572675e0d61b2d3476e1
Parker A. Norton <pnorton@usgs.gov>
2016-12-21 09:23:09 -0700

    - This version works -- prior to creating new classes

--- cbh.py
+++ cbh.py
@@ -1,9 +1,12 @@
+from __future__ import (absolute_import, division, print_function)
+from future.utils import iteritems
 
 import calendar
 from datetime import datetime
 import pandas as pd
 
 CBH_VARNAMES = ['prcp', 'tmin', 'tmax']
+CBH_INDEX_COLS = [0, 1, 2, 3, 4, 5]
 
 
 def dparse(*dstr):
@@ -42,15 +45,18 @@ class Cbh(object):
     # took care of those corrections itself. This would provide a more seamless workflow
     # from GDP to PRMS. At this point I'm not taking this on though -- for a future revision.
 
-    def __init__(self, filename, st_date=None, en_date=None, indices=None):
+    def __init__(self, filename=None, st_date=None, en_date=None, indices=None, nhm_hrus=None, mapping=None):
+    # def __init__(self, cbhdb_dir, st_date=None, en_date=None, indices=None, nhm_hrus=None, mapping=None):
+    #     self.__cbhdb_dir = cbhdb_dir
         self.__filename = filename
         self.__indices = indices    # This should be an ordered dict: nhm->local hrus
         self.__stdate = st_date
         self.__endate = en_date
+        self.__nhm_hrus = nhm_hrus
+        self.__mapping = mapping
         self.__date_range = None
         self.__data = None
         self.__final_outorder = None
-
 
     @property
     def data(self):
@@ -85,7 +91,7 @@ class Cbh(object):
 
     def read_cbh2(self):
         if self.__indices:
-            incl_cols = [0, 1, 2, 3, 4, 5]
+            incl_cols = CBH_INDEX_COLS.copy()
             for xx in self.__indices.values():
                 incl_cols.append(xx+5)  # include an offset for the having datetime info
 
@@ -103,7 +109,40 @@ class Cbh(object):
             # NOTE: The rename is an expensive operation
             self.__data.rename(columns=ren_dict, inplace=True)
         else:
+            # NOTE: This is incomplete and won't work properly without code to rename to columns
+            #       to national HRU ids.
+
             # Load the CBH file
             self.__data = pd.read_csv(self.__filename, sep=' ', skipinitialspace=True, skiprows=3, engine='c',
                                       date_parser=dparse, parse_dates={'thedate': [0, 1, 2, 3, 4, 5]},
                                       index_col='thedate', header=None, na_values=[-99.0, -999.0])
+
+    def write_cbh_subset(self, outdir):
+        outdata = None
+        first = True
+
+        for vv in CBH_VARNAMES:
+            outorder = CBH_INDEX_COLS.copy()
+
+            for rr, rvals in iteritems(self.__mapping):
+                idx_retrieve = {}
+
+                for yy in self.__nhm_hrus.keys():
+                    if rvals[0] <= yy <= rvals[1]:
+                        idx_retrieve[yy] = self.__nhm_hrus[yy]
+
+                if len(idx_retrieve) > 0:
+                    self.__filename = '{}/{}_{}.cbh.gz'.format(self.__cbhdb_dir, rr, vv)
+                    self.read_cbh2()
+                    if first:
+                        outdata = self.__data
+                        first = False
+                    else:
+                        outdata = pd.merge(outdata, self.__data, how='left', left_index=True, right_index=True)
+
+            # Append the HRUs as ordered for the subset
+            outorder.extend(self.__nhm_hrus)
+
+            out_cbh = open('{}/{}.cbh'.format(outdir, vv), 'w')
+            out_cbh.write('Written by Skein\n')
+            out_cbh.write('{} {}\n'.format(vv, len()))
--- checkout.py
+++ checkout.py
@@ -8,8 +8,6 @@ import networkx as nx
 import numpy as np
 import pandas as pd
 import msgpack
-import ogr
-import re
 import sys
 
 from collections import OrderedDict
