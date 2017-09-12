59f7be86dd32e3b2ddc539ae87854e89395ae2b8
Parker A. Norton <pnorton@usgs.gov>
2017-09-12 07:05:57 -0600

    - fixed problem with bandit_cfg.py where yaml would inconistently convert to datetime objects when it shouldn't. Dates should be handled as strings when read from the config file; they are converted to datetime objects later.

--- bandit_cfg.py
+++ bandit_cfg.py
@@ -11,6 +11,31 @@ from future.utils import iteritems
 import yaml
 
 
+# Following class from: https://stackoverflow.com/questions/34667108/ignore-dates-and-times-while-parsing-yaml
+class NoDatesSafeLoader(yaml.SafeLoader):
+    @classmethod
+    def remove_implicit_resolver(cls, tag_to_remove):
+        """
+        Remove implicit resolvers for a particular tag
+
+        Takes care not to modify resolvers in super classes.
+
+        We want to load datetimes as strings, not dates, because we
+        go on to serialise as json which doesn't have the advanced types
+        of yaml, and leads to incompatibilities down the track.
+        """
+        if 'yaml_implicit_resolvers' not in cls.__dict__:
+            cls.yaml_implicit_resolvers = cls.yaml_implicit_resolvers.copy()
+
+        for first_letter, mappings in cls.yaml_implicit_resolvers.items():
+            cls.yaml_implicit_resolvers[first_letter] = [(tag, regexp)
+                                                         for tag, regexp in mappings
+                                                         if tag != tag_to_remove]
+
+
+NoDatesSafeLoader.remove_implicit_resolver('tag:yaml.org,2002:timestamp')
+
+
 class Cfg(object):
     def __init__(self, filename, cmdline=None):
         self.__cfgdict = None
@@ -45,7 +70,7 @@ class Cfg(object):
             return None
 
     def load(self, filename):
-        tmp = yaml.load(open(filename, 'r'))
+        tmp = yaml.load(open(filename, 'r'), Loader=NoDatesSafeLoader)
         self.__cfgdict = tmp
 
     def update_value(self, variable, newval):
