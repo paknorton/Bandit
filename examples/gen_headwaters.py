be1cbfe740df20d275b7a39074e349fa362fb99a
Parker A. Norton <pnorton@usgs.gov>
2018-09-19 10:55:46 -0600

    Reorganized code; dictionary of headwaters can now have string keys.

--- examples/gen_headwaters.py
+++ examples/gen_headwaters.py
@@ -7,7 +7,8 @@ import os
 import subprocess32 as subprocess
 import sys
 from collections import OrderedDict
-import bandit_cfg as bc
+from Bandit import bandit_cfg as bc
+
 
 import time
 import threading
@@ -23,6 +24,7 @@ hwAreaId, seg_id_nat, [variable length list of seg_id_nat]
 2,18, 16
 
 """
+
 
 class WorkerThread(threading.Thread):
     """ A worker thread that takes directory names from a queue, finds all
@@ -74,111 +76,122 @@ class WorkerThread(threading.Thread):
         return retcode
 
 
-hw_src = '/media/scratch/PRMS/bandit/jobs/hw_jobs/hwSegsVll.csv'
+def main():
+    jobdir = '/media/scratch/PRMS/bandit/jobs/hw_jobs'
+    hw_src = '/media/scratch/PRMS/bandit/jobs/hw_jobs/hwSegsVll.csv'
+    default_config_file = '/media/scratch/PRMS/bandit/jobs/hw_jobs/bandit.cfg'
+    cmd_bandit = '/media/scratch/PRMS/bandit/Bandit/bandit.py'
 
-hw_file = open(hw_src, 'r')
-hw_file.next()
+    hwdir = 'hw_{:04d}'
 
-# First column is hwAreaId
-# Second and following columns are seg_id_nat
-head_waters = OrderedDict()
+    hw_file = open(hw_src, 'r')
 
-for line in hw_file:
-    cols = line.strip().replace(" ", "").split(',')
-    cols = [int(xx) for xx in cols]
-    head_waters[cols[0]] = cols[1:]
+    # Skip the header information
+    # NOTE: If file has no header the first entry will be skipped
+    hw_file.next()
 
-num_threads = 8
+    # First column is hwAreaId
+    # Second and following columns are seg_id_nat
+    head_waters = OrderedDict()
 
-# ****************************************************************************
-# Initialize the threads
-cmd_q = Queue.Queue()
-result_q = Queue.Queue()
+    for line in hw_file:
+        cols = line.strip().replace(" ", "").split(',')
+        try:
+            # Assume first column is a number
+            cols = [int(xx) for xx in cols]
+            head_waters[cols[0]] = cols[1:]
+        except ValueError:
+            # First column is probably a string
+            head_waters[cols[0]] = [int(xx) for xx in cols[1:]]
 
-# Create pool of threads
-pool = [WorkerThread(input_q=cmd_q, result_q=result_q) for i in range(num_threads)]
+    num_threads = 8
 
-# Start the threads
-for thread in pool:
-    try:
-        thread.start()
-    except (KeyboardInterrupt, SystemExit):
-        # Shutdown the threads when the program is terminated
-        thread.join()
-        sys.exit(1)
+    # ****************************************************************************
+    # Initialize the threads
+    cmd_q = Queue.Queue()
+    result_q = Queue.Queue()
 
+    # Create pool of threads
+    pool = [WorkerThread(input_q=cmd_q, result_q=result_q) for __ in range(num_threads)]
 
-# ### For each head_waters
-# - create directory hw_# (where # is the hwAreaId)
-# - copy default bandit.cfg into directory
-# - run bandit on the directory
-# 
-default_config_file = '/media/scratch/PRMS/bandit/jobs/hw_jobs/bandit.cfg'
+    # Start the threads
+    for thread in pool:
+        try:
+            thread.start()
+        except (KeyboardInterrupt, SystemExit):
+            # Shutdown the threads when the program is terminated
+            thread.join()
+            sys.exit(1)
 
-jobdir = '/media/scratch/PRMS/bandit/jobs/hw_jobs'
-hwdir = 'hw_{:04d}'
+    # ### For each head_waters
+    # - create directory hw_# (where # is the hwAreaId)
+    # - copy default bandit.cfg into directory
+    # - run bandit on the directory
+    #
 
-cmd_bandit = '/media/scratch/PRMS/bandit/Bandit/bandit.py'
-
-if not os.path.exists(jobdir):
-    try:
-        os.makedirs(jobdir)
-    except OSError as err:
-        print("\tError creating directory: {}".format(err))
-        exit(1)
-        
-st_dir = os.getcwd()
-os.chdir(jobdir)
-
-# Read the default configuration file
-config = bc.Cfg(default_config_file)
-
-work_count = 0
-
-for kk, vv in iteritems(head_waters):
-    cdir = hwdir.format(kk)
-
-    # Create the headwater directory if needed
-    if not os.path.exists(cdir):
-        try: 
-            os.makedirs(cdir)
+    if not os.path.exists(jobdir):
+        try:
+            os.makedirs(jobdir)
         except OSError as err:
             print("\tError creating directory: {}".format(err))
             exit(1)
-            
-    # Update the outlets in the basin.cfg file and write into the headwater directory
-    config.update_value('outlets', vv)
-    config.update_value('output_dir', '{}/{}'.format(jobdir, cdir))
-    config.write('{}/bandit.cfg'.format(cdir))
-    
-    # Run bandit
-    # Add the command to queue for processing
-    work_count += 1
-    cmd = '{} -j {}/{}'.format(cmd_bandit, jobdir, cdir)
 
-    os.chdir(cdir)
-    cmd_q.put(cmd)
+    st_dir = os.getcwd()
     os.chdir(jobdir)
-    
-#    if work_count == 6:
-#        break
-        
-print("work_count = {:d}".format(work_count))
 
-# Output results
-while work_count > 0:
-    result = result_q.get()
+    # Read the default configuration file
+    config = bc.Cfg(default_config_file)
 
-    sys.stdout.write("\rwork_count: {:4d}".format(work_count))
-    sys.stdout.flush()
-    
-#     print "Thread %s return code = %d" % (result[0], result[2])
-    work_count -= 1
-    
-    if result[2] != 0:
-        # An error occurred running the command
-        work_count = 0
+    work_count = 0
 
-# Ask for the threads to die and wait for them to do it
-for thread in pool:
-    thread.join()
+    for kk, vv in iteritems(head_waters):
+        cdir = hwdir.format(kk)
+
+        # Create the headwater directory if needed
+        if not os.path.exists(cdir):
+            try:
+                os.makedirs(cdir)
+            except OSError as err:
+                print("\tError creating directory: {}".format(err))
+                exit(1)
+
+        # Update the outlets in the basin.cfg file and write into the headwater directory
+        config.update_value('outlets', vv)
+        config.update_value('output_dir', '{}/{}'.format(jobdir, cdir))
+        config.write('{}/bandit.cfg'.format(cdir))
+
+        # Run bandit
+        # Add the command to queue for processing
+        work_count += 1
+        cmd = '{} -j {}/{}'.format(cmd_bandit, jobdir, cdir)
+
+        os.chdir(cdir)
+        cmd_q.put(cmd)
+        os.chdir(jobdir)
+
+    #    if work_count == 6:
+    #        break
+
+    print("work_count = {:d}".format(work_count))
+
+    # Output results
+    while work_count > 0:
+        result = result_q.get()
+
+        sys.stdout.write("\rwork_count: {:4d}".format(work_count))
+        sys.stdout.flush()
+
+    #     print "Thread %s return code = %d" % (result[0], result[2])
+        work_count -= 1
+
+        if result[2] != 0:
+            # An error occurred running the command
+            work_count = 0
+
+    # Ask for the threads to die and wait for them to do it
+    for thread in pool:
+        thread.join()
+
+
+if __name__ == '__main__':
+    main()
