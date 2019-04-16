6f45f337eb1aef078c5e5872ddc3c440d804443d
Parker A. Norton <pnorton@usgs.gov>
2019-04-16 08:48:07 -0600

    Remove xml package_data

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={'Bandit': ['xml/*.xml'],
+    package_data={
         # 'sample': ['package_data.dat'],
     },
 
@@ -119,6 +119,6 @@ setup(
             'validate_parameters=Bandit.validate_parameters:main'
         ],
     },
-    
+
     zip_safe=False,
 )
