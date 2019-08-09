474858888fabe75e414c69b8627e87769ee76c3d
Parker A. Norton <pnorton@usgs.gov>
2019-08-09 09:21:12 -0600

    Added model output bandit script

--- setup.py
+++ setup.py
@@ -112,6 +112,7 @@ setup(
             'bandit=Bandit.bandit:main',
             'bandit_v2=Bandit.bandit_v2:main',
             'bandit_by_streamgage=Bandit.bandit_by_streamgage:main',
+            'bandit_model_output=Bandit.bandit_paramset:main',
             'create_merged_db=Bandit.create_merged_db:main',
             'git_version=Bandit.git_version:main',
             'create_bandit_job=Bandit.utilities.create_bandit_job:main',
