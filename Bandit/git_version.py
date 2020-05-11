5d7a5f25dac5fd58b390307b23b6dd6e21e902eb
Parker Norton <pnorton@usgs.gov>
2020-05-11 08:33:02 -0600

    Add additional functions for repo information

--- Bandit/git_version.py
+++ Bandit/git_version.py
@@ -28,20 +28,32 @@ def _minimal_ext_cmd(cmd):
     return result
 
 
-def git_version(repo_dir):
+def git_commit_url(repo_dir):
+    src_url = git_repo(repo_dir)
+    src_commit = git_commit(repo_dir)
+
+    dst_url = src_url.replace(':', '/').replace('.git', '').replace('git@', 'https://')
+    dst_url += f'/commit/{src_commit}'
+    return dst_url
+
+
+def git_commit(repo_dir, length=None):
     """Retrieve current git version from local directory.
 
     Args:
         repo_dir (str): Local git repository directory.
 
-    Returns:
+   aa Returns:
         str: Current git revision or 'Unknown'.
 
     """
 
     # Return the git revision as a string
     try:
-        out = _minimal_ext_cmd(['git', '-C', repo_dir, 'rev-parse', 'HEAD'])
+        if length is None:
+            out = _minimal_ext_cmd(['git', '-C', repo_dir, 'rev-parse', 'HEAD'])
+        else:
+            out = _minimal_ext_cmd(['git', '-C', repo_dir, 'rev-parse', f'--short={length}', 'HEAD'])
         git_revision = out.strip().decode('ascii')
     except OSError:
         git_revision = "Unknown"
@@ -50,7 +62,7 @@ def git_version(repo_dir):
 
 
 def git_repo(repo_dir):
-    # Return the git revision as a string
+    # Return the git repo URL as a string
     try:
         out = _minimal_ext_cmd(['git', '-C', repo_dir, 'config', '--get', 'remote.origin.url'])
         git_repo_url = out.strip().decode('ascii')
@@ -61,7 +73,7 @@ def git_repo(repo_dir):
 
 
 def git_branch(repo_dir):
-    # Return the git revision as a string
+    # Return the git branch as a string
     try:
         out = _minimal_ext_cmd(['git', '-C', repo_dir, 'rev-parse', '--abbrev-ref', 'HEAD'])
         git_repo_branch = out.strip().decode('ascii')
@@ -86,7 +98,7 @@ def main():
 
     print(f'GIT repo URL: {git_repo(args.path_repo)}')
     print(f'GIT branch: {git_branch(args.path_repo)}')
-    print(f'GIT commit: {git_version(args.path_repo)}')
+    print(f'GIT commit: {git_commit(args.path_repo)}')
 
 
 if __name__ == '__main__':
