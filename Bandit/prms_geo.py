d144606bdeddc06924ca78578cbed55d926a2469
Parker Norton <pnorton@usgs.gov>
2020-05-13 15:59:15 -0600

    Code cleanup; convert to f-strings

--- Bandit/prms_geo.py
+++ Bandit/prms_geo.py
@@ -1,9 +1,4 @@
 
-
-# from __future__ import (absolute_import, division, print_function)
-# , unicode_literals)
-# from future.utils import iteritems
-
 from collections import OrderedDict
 from osgeo import ogr
 import gdal
@@ -115,7 +110,7 @@ class Geo(object):
         print(attr_args)
         print('-' * 50)
 
-        self.__selected_layer.SetAttributeFilter('{} in ({})'.format(attr_name, attr_args))
+        self.__selected_layer.SetAttributeFilter(f'{attr_name} in ({attr_args})')
 
     def write_shapefile(self, filename, attr_name, attr_values, included_fields=None):
         """Write subset to shapefile format.
@@ -172,9 +167,11 @@ class Geo(object):
 
                     if fld_name == 'model_idx':
                         # Output a 1-based array index value
-                        out_feat.SetField(out_layer_def.GetFieldDefn(ii).GetNameRef(), attr_values.index(in_feat.GetField(attr_name))+1)
+                        out_feat.SetField(out_layer_def.GetFieldDefn(ii).GetNameRef(),
+                                          attr_values.index(in_feat.GetField(attr_name))+1)
                     else:
-                        out_feat.SetField(out_layer_def.GetFieldDefn(ii).GetNameRef(), in_feat.GetField(orig_fld_names[ii]))
+                        out_feat.SetField(out_layer_def.GetFieldDefn(ii).GetNameRef(),
+                                          in_feat.GetField(orig_fld_names[ii]))
 
                 # Set geometry as centroid
                 # geom = in_feat.GetGeometryRef
@@ -200,67 +197,67 @@ class Geo(object):
         out_ds.CopyLayer(self.__selected_layer, self.__selected_layer.GetName())
         del out_ds
 
-    def write_shapefile3(self, filename, attr_name, attr_values, included_fields=None):
-        # NOTE: This is for messing around with the geopackage format
-
-        # Create a shapefile for the current selected layer
-        # If a filter is set then a subset of features is written
-        print(attr_values)
-        out_driver = ogr.GetDriverByName('GPKG')
-
-        out_ds = out_driver.CreateDataSource('crap.gpkg')
-        # out_ds = out_driver.CreateDataSource(filename)
-        out_layer = out_ds.CreateLayer(self.__selected_layer.GetName(), self.__selected_layer.GetSpatialRef())
-
-        # Copy field definitions from input to output file
-        in_layer_def = self.__selected_layer.GetLayerDefn()
-
-        for ii in range(in_layer_def.GetFieldCount()):
-            fld_def = in_layer_def.GetFieldDefn(ii)
-            fld_name = fld_def.GetName()
-
-            if included_fields and fld_name not in included_fields:
-                continue
-            out_layer.CreateField(fld_def)
-
-        # Get feature definitions for the output layer
-        out_layer_def = out_layer.GetLayerDefn()
-
-        # Create blank output feature
-        out_feat = ogr.Feature(out_layer_def)
-
-        # Add features to the output layer
-        for in_feat in self.__selected_layer:
-            if in_feat.GetField(attr_name) in attr_values:
-                print(in_feat.GetField(attr_name))
-                # Add field values from the input layer
-                for ii in range(out_layer_def.GetFieldCount()):
-                    fld_def = out_layer_def.GetFieldDefn(ii)
-                    fld_name = fld_def.GetName()
-
-                    if included_fields and fld_name not in included_fields:
-                        continue
-                    out_feat.SetField(out_layer_def.GetFieldDefn(ii).GetNameRef(), in_feat.GetField(ii))
-
-                # Set geometry as centroid
-                # geom = in_feat.GetGeometryRef()
-                # out_feat.SetGeometry(geom.Clone())
-
-                # Set geometry
-                geom = in_feat.geometry()
-                out_feat.SetGeometry(geom)
-
-                # Add the new feature to the output layer
-                out_layer.CreateFeature(out_feat)
-
-        # Close the output datasource
-        out_ds.Destroy()
-
-    def write_kml(self, filename):
-        # Create a shapefile for the current selected layer
-        # Any applied filter will effect what is written to the new file
-        out_driver = ogr.GetDriverByName('KML')
-        out_ds = out_driver.CreateDataSource(filename)
-
-        out_ds.CopyLayer(self.__selected_layer, self.__selected_layer.GetName())
-        del out_ds
+    # def write_shapefile3(self, filename, attr_name, attr_values, included_fields=None):
+    #     # NOTE: This is for messing around with the geopackage format
+    #
+    #     # Create a shapefile for the current selected layer
+    #     # If a filter is set then a subset of features is written
+    #     print(attr_values)
+    #     out_driver = ogr.GetDriverByName('GPKG')
+    #
+    #     out_ds = out_driver.CreateDataSource('crap.gpkg')
+    #     # out_ds = out_driver.CreateDataSource(filename)
+    #     out_layer = out_ds.CreateLayer(self.__selected_layer.GetName(), self.__selected_layer.GetSpatialRef())
+    #
+    #     # Copy field definitions from input to output file
+    #     in_layer_def = self.__selected_layer.GetLayerDefn()
+    #
+    #     for ii in range(in_layer_def.GetFieldCount()):
+    #         fld_def = in_layer_def.GetFieldDefn(ii)
+    #         fld_name = fld_def.GetName()
+    #
+    #         if included_fields and fld_name not in included_fields:
+    #             continue
+    #         out_layer.CreateField(fld_def)
+    #
+    #     # Get feature definitions for the output layer
+    #     out_layer_def = out_layer.GetLayerDefn()
+    #
+    #     # Create blank output feature
+    #     out_feat = ogr.Feature(out_layer_def)
+    #
+    #     # Add features to the output layer
+    #     for in_feat in self.__selected_layer:
+    #         if in_feat.GetField(attr_name) in attr_values:
+    #             print(in_feat.GetField(attr_name))
+    #             # Add field values from the input layer
+    #             for ii in range(out_layer_def.GetFieldCount()):
+    #                 fld_def = out_layer_def.GetFieldDefn(ii)
+    #                 fld_name = fld_def.GetName()
+    #
+    #                 if included_fields and fld_name not in included_fields:
+    #                     continue
+    #                 out_feat.SetField(out_layer_def.GetFieldDefn(ii).GetNameRef(), in_feat.GetField(ii))
+    #
+    #             # Set geometry as centroid
+    #             # geom = in_feat.GetGeometryRef()
+    #             # out_feat.SetGeometry(geom.Clone())
+    #
+    #             # Set geometry
+    #             geom = in_feat.geometry()
+    #             out_feat.SetGeometry(geom)
+    #
+    #             # Add the new feature to the output layer
+    #             out_layer.CreateFeature(out_feat)
+    #
+    #     # Close the output datasource
+    #     out_ds.Destroy()
+    #
+    # def write_kml(self, filename):
+    #     # Create a shapefile for the current selected layer
+    #     # Any applied filter will effect what is written to the new file
+    #     out_driver = ogr.GetDriverByName('KML')
+    #     out_ds = out_driver.CreateDataSource(filename)
+    #
+    #     out_ds.CopyLayer(self.__selected_layer, self.__selected_layer.GetName())
+    #     del out_ds
