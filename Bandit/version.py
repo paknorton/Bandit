eecee1e4695412e867ad72a4e52239deea8e1fca
Parker Norton <pnorton@usgs.gov>
2023-11-13 09:54:37 -0700

    Initial commit

---
+++ Bandit/version.py
@@ -0,0 +1,13 @@
+major = 0
+minor = 8
+micro = 7
+__version__ = f"{major}.{minor}.{micro}"
+
+__pakname__ = "Bandit"
+
+# edit author dictionary as necessary (in order of commits after Bakker and Post)
+author_dict = {
+    "Parker Norton": "pnorton@usgs.gov",
+}
+__author__ = ", ".join(author_dict.keys())
+__author_email__ = ", ".join(s for _, s in author_dict.items())
