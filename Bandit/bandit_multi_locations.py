a73235eb10d414f4ac1e1bc04477186cff7d5980
Parker Norton <pnorton@usgs.gov>
2024-06-04 08:44:41 -0600

    Remove check for zero-length filename

--- Bandit/bandit_multi_locations.py
+++ Bandit/bandit_multi_locations.py
@@ -80,20 +80,19 @@ def read_file(filename):
 
     values_by_id = OrderedDict()
 
-    if len(filename) > 0:
-        src_file = open(filename, 'r')
-        src_file.readline()
+    src_file = open(filename, 'r')
+    src_file.readline()
 
-        # Read in the non-routed HRUs by location
-        for line in src_file:
-            cols = line.strip().replace(' ', '').split(',')
-            try:
-                # Assume first column is a number
-                cols = [int(xx) for xx in cols]
-                values_by_id[cols[0]] = cols[1:]
-            except ValueError:
-                # First column is probably a string
-                values_by_id[cols[0]] = [int(xx) for xx in cols[1:]]
+    # Read in the non-routed HRUs by location
+    for line in src_file:
+        cols = line.strip().replace(' ', '').split(',')
+        try:
+            # Assume first column is a number
+            cols = [int(xx) for xx in cols]
+            values_by_id[cols[0]] = cols[1:]
+        except ValueError:
+            # First column is probably a string
+            values_by_id[cols[0]] = [int(xx) for xx in cols[1:]]
     return values_by_id
 
 
