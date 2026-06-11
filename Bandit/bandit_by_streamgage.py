985b5bda9a8370af63a00c4170834f94b3f208c0
Parker Norton <pnorton@usgs.gov>
2026-06-11 06:08:02 -0600

    Modify bandit call to use job-dir argument

--- Bandit/bandit_by_streamgage.py
+++ Bandit/bandit_by_streamgage.py
@@ -187,7 +187,7 @@ def main():
 
         # Add bandit job to the processing queue
         work_count += 1
-        cmd = f'{cmd_bandit} --no_filter_params --keep_hru_order -j {job_dir}/{cdir}'
+        cmd = f'{cmd_bandit} --no_filter_params --keep_hru_order --job-dir {job_dir}/{cdir}'
 
         cmd_q.put(cmd)
 
